55ce6660c03fb16b554318a0dc4259653d1c8abc
Frederick Cheung <frederick.cheung@gmail.com>
2011-07-20 08:26:22 +0100

    fix typo

--- lib/fog/aws/parsers/auto_scaling/describe_launch_configurations.rb
+++ lib/fog/aws/parsers/auto_scaling/describe_launch_configurations.rb
@@ -39,7 +39,7 @@ module Fog
           def end_element(name)
             
             case name
-            case 'CreatedTime'
+            when 'CreatedTime'
               @launch_configuration[name] = Time.parse(value)
             when 'DeviceName', 'VirtualDevice'
               @block_device_mapping[name] = value
